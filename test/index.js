9b69feeaefdbe5eed9384f877ba973e720663445
ForbesLindesay <forbes@lindesay.co.uk>
2013-08-01 05:30:05 +0100

    Initial Code

---
+++ test/index.js
@@ -0,0 +1,21 @@
+var fs = require('fs')
+var br = require('../')
+var test = fs.readFileSync(require.resolve('uglify-js/test/run-tests.js'), 'utf8')
+  .replace(/^#.*\n/, '')
+
+br(require.resolve('uglify-js'))
+  .pipe(fs.createWriteStream(__dirname + '/output.js'))
+  .on('close', function () {
+    Function('module,require', test)({
+      filename: require.resolve('uglify-js/test/run-tests.js')
+    },
+      function (name) {
+        if (name === '../tools/node') {
+          return require('./output.js')
+        } else if (/^[a-z]+$/.test(name)) {
+          return require(name)
+        } else {
+          throw new Error('I didn\'t expect you to require ' + name)
+        }
+      })
+  })
